cb13d392eba6dee0136ad9c3dc4bc6817e331755
Adi Album <adi.album1@gmail.com>
2021-02-06 12:28:33 +0200

    Update documentation

--- detector.py
+++ detector.py
@@ -18,14 +18,19 @@ class MyMotionDetector:
         self._close_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (self._params.close_size, self._params.close_size))
 
     def __call__(self, frame):
+        # Apply motion detection algorithm
         mask = self._bgs.apply(frame)
+        # Threshold to keep only foreground: 0 - background, 127 - shadow, 255 - foreground
         _, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
+        # Filter noise using Median filter and morphological operations Open & Close.
         mask = ((cv2.boxFilter(mask,cv2.CV_8U,(self._params.median,self._params.median)) > 255*0.3)*255).astype(np.uint8)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self._open_kernel)  # remove small objects
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, self._close_kernel)  # fill the holes
 
+        # Connected Components Algorithm
         num_comp, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=self._params.connectivity)
 
+        # Define our list of objects and insert every valid object
         objects = []
         for n in range(1, num_comp):
             new_object = Object()
